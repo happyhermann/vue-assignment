1a370582cdc21591363ce61d17ec337558823339
happyhermann <carrick035@gmail.com>
2022-10-10 15:52:21 +0900

    Add agency information
    when page mounted using beforeMount();

--- src/components/MyPartner.vue
+++ src/components/MyPartner.vue
@@ -2,24 +2,97 @@
 
 <template>
     <v-container>
-      <v-row class="text-center">
+       <v-row class="text-center">
         <img src="https://cdn.quv.kr/oluwww1tm%2Fup%2F63218781ee0b0_1920.png" />
           <p>파트너님! 로그인 성공하였습니다</p>
-      </v-row>
+       </v-row>
+      <LodingSpinner/>
     </v-container>
+    <v-col
+        class="d-flex"
+        cols="12"
+        sm="6"
+      >
+        <v-select
+          :items="items"
+          label="리스트"
+          solo
+        ></v-select>
+        
+      </v-col>
+ 
+    <!-- <buttom v-on:click="onClick">테스트 클릭</buttom> -->
+     <!-- <h4>{{this.$store.state.isMember}}</h4> -->
   </template>
   
   <script>
+
+  /* eslint-disable */ 
+  import axios from "axios"
+
+  import LodingSpinner from "../components/LodingSpinner"
+  
+  var memberNo = 0;
+  var some = "";
+  var self = this;
   
   export default {
-    name: 'MyPartner',
+
   
-    data: () => ({
-     
-    }),
+    data: function () {
+      return {
+        items: [""],
+
+      }
+    },
+    
+    components : {
+      LodingSpinner,
+     },  
+
+     created() {
+      memberNo = this.$route.params.id;
+      // url에서 id 추출하기 
+      },
+
+      methods: {
+        // 마운트시 자동으로 axios 불러오는 함수 띄우기
+        // 리액트에서 useEffect같은 것인가?
+        
+        onAxios : function () {
+          let self = this;
+ 
+          const url = `/v1/member/getCo_Mem_Jj.php`;
+          const data = {
+            "mode":"myAgencyList",
+            "coMemberKey": 60, 
+            "mb_no": memberNo,
+          }
+
+          axios.post(url, data).
+          then(function(res) {
+          some = res.data.agencyList[0].addr1;
+          self.items.unshift(some);
+
+            console.log(res)
+           
+   
+          }).
+          catch(function(error) {
+            console.log(error);
+          });
+        },
+      },
+      beforeMount() {
+        this.onAxios();
+      },
+      // 페이지로드시 안에 있는 것들을 실행해줌
+   
+      
+
   }
   </script>
-  
+   
 
     
   <style>
