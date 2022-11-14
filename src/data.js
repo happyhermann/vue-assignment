9fb20d6ada3fe902d80eea84e69de3123d1a5f85
Chiyun Lee <carrick035@gmail.com>
2022-11-14 16:25:29 +0900

    윈도우 -> 맥

--- src/data.js
+++ src/data.js
@@ -1,24 +1,39 @@
-const data = [
+const logos = [
       {
             id: 0,
-            title: "10만원",
-            image: "http://ojsfile.ohmynews.com/STD_IMG_FILE/2017/0901/IE002211389_STD.jpg",
+            title: "파리바게트",
+            image: "https://www.paris.co.kr/wp-content/uploads/PB_Marque_Emblem_Standard_Reversed.blue_.png",
       },
       {
             id: 1,
-            title: "꽝",
-            image: "http://cdn.iconsumer.or.kr/news/photo/201705/3122_2309_156.jpg",
+            title: "맘스터치",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Mom%27s_Touch_logo_%282020%29.svg/1200px-Mom%27s_Touch_logo_%282020%29.svg.png",
       },
       {
             id: 2,
-            title: "박카스",
+            title: "샤니",
             image: "https://www.kfoodtimes.com/news/photo/202205/20238_30782_3243.png",
       },
       {
             id: 3,
-            title: "123",
+            title: "CU",
+            image: "https://mblogthumb-phinf.pstatic.net/20111216_260/bandi1649_1324021125100Xfjqu_JPEG/%C7%C1%B7%A3%C2%F7%C0%CC%C1%EE%B7%CE%B0%ED%B8%F0%C0%BD_%C0%BD%BD%C4%C1%A1%B7%CE%B0%ED%B8%F0%C0%BD_01.jpg?type=w2",
+      },
+      {
+            id: 4,
+            title: "자담치킨",
+            image: "https://mblogthumb-phinf.pstatic.net/20111216_260/bandi1649_1324021125100Xfjqu_JPEG/%C7%C1%B7%A3%C2%F7%C0%CC%C1%EE%B7%CE%B0%ED%B8%F0%C0%BD_%C0%BD%BD%C4%C1%A1%B7%CE%B0%ED%B8%F0%C0%BD_01.jpg?type=w2",
+      },
+      {
+            id: 5,
+            title: "노랑통닭",
+            image: "https://mblogthumb-phinf.pstatic.net/20111216_260/bandi1649_1324021125100Xfjqu_JPEG/%C7%C1%B7%A3%C2%F7%C0%CC%C1%EE%B7%CE%B0%ED%B8%F0%C0%BD_%C0%BD%BD%C4%C1%A1%B7%CE%B0%ED%B8%F0%C0%BD_01.jpg?type=w2",
+      },
+      {
+            id: 6,
+            title: "피자헛",
             image: "https://mblogthumb-phinf.pstatic.net/20111216_260/bandi1649_1324021125100Xfjqu_JPEG/%C7%C1%B7%A3%C2%F7%C0%CC%C1%EE%B7%CE%B0%ED%B8%F0%C0%BD_%C0%BD%BD%C4%C1%A1%B7%CE%B0%ED%B8%F0%C0%BD_01.jpg?type=w2",
       },
 ];
 
-export default data;
+export default logos;
