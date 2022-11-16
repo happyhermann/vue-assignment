9c67db4fa03623fb8f7a30dac0b1c7af2f951ef0
LeeChiYun <carrick035@gmail.com>
2022-11-16 10:23:15 +0900

    QR 자동 종료 시 로딩중 띄우기 셋타임 마무리

--- src/carbon/QR.vue
+++ src/carbon/QR.vue
@@ -1,379 +1,351 @@
-<template >
-     
-    <div v-if="switch" class="qr-container">
-        <div class="qr-white-bg">
-            <header class="qr-header">
-                <i class="mdi mdi-close qrClose" @click="[$emit('close'), onClose()]"></i>
-                <!-- 이거 누르면 모달 자체를 끄게 -->
+<template>
+      <div v-if="switch" class="qr-container">
+            <div class="qr-white-bg">
+                  <header class="qr-header">
+                        <i
+                              class="mdi mdi-close qrClose"
+                              @click="[$emit('close'), onClose()]"
+                        ></i>
+                        <!-- 이거 누르면 모달 자체를 끄게 -->
 
-                <span>탄소중립저울</span>
-             </header>
-             <div class="item" data-aos="zoom-in">
-             <main class="qr-main">
-                <div class="qr-guide">
-                    <i class="mdi mdi-information-outline"></i>
-                    <span>도움이 필요해요</span>
-                </div>
-                <div class="qr-image-box">
-                    <img class="qr" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png" alt="QR 코드"/>
-                </div>
-                <div class="qr-bottom">
-                     <p class="qr-timer">{{this.minutes}} : {{this.seconds}} </p>    
-                    <button v-if="isDone"  @click="allDone()"  class="qr-done-btn ">배출완료</button>
+                        <span>탄소중립저울</span>
+                  </header>
+                  <div class="item" data-aos="zoom-in">
+                        <main class="qr-main">
+                              <div class="qr-guide">
+                                    <i class="mdi mdi-information-outline"></i>
+                                    <span>도움이 필요해요</span>
+                              </div>
+                              <div class="qr-image-box">
+                                    <img
+                                          class="qr"
+                                          src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png"
+                                          alt="QR 코드"
+                                    />
+                              </div>
+                              <div class="qr-bottom">
+                                    <p class="qr-timer">
+                                          {{ minutes }} : {{ seconds }}
+                                    </p>
+                                    <button
+                                          v-if="isDone"
+                                          class="qr-done-btn"
+                                          @click="allDone()"
+                                    >
+                                          배출완료
+                                    </button>
+                              </div>
+                        </main>
                   </div>
-            </main>
             </div>
       </div>
-    </div>
-    <!-- 배출완료 것 조건부 렌더링 -->
-    <div v-else class="qr-container">
+      <!-- 배출완료 것 조건부 렌더링 -->
+      <div v-else class="qr-container">
+            <div class="qr-white-bg">
+                  <div class="item" data-aos="flip-right">
+                        <header class="qr-header">
+                              <i
+                                    class="mdi mdi-close qrClose"
+                                    @click="[$emit('close'), onClose()]"
+                              ></i>
+                              <!-- 이거 누르면 모달 자체를 끄게 -->
 
-        <div class="qr-white-bg">
-            
-            <div class="item" data-aos="flip-right">
-
-            <header class="qr-header">
-                <i class="mdi mdi-close qrClose" @click="[$emit('close'), onClose()]"></i>
-                <!-- 이거 누르면 모달 자체를 끄게 -->
-
-                <span>탄소중립저울</span>
-            </header>
-            </div>
-            <div class="item" data-aos="flip-right">
-
-            <main class="qr-main">
-                <div class="qr-done-guide">
-                    <p class="qr-done-text">배출 완료!</p>
-                </div>
-                <div class="qr-done-image-box">
-                    <img class="allsu-logo-3d"  :src="require(`../assets/allsu-logo-3d.png`)" alt="올수 로고"/>
-                </div>
-                <div class="qr-done-details">
-                    <span>오늘의 배출량은  <strong>1300mL </strong>이며</span>
-                    <br/><span>적립 금액은  <strong>130원 </strong>입니다</span>
-                </div>
-                <div class="qr-bottom">
-                     <button @click="[allDone(), $emit('close')]"  class="qr-done-btn ">확인</button>
-                </div>
-            </main>
+                              <span>탄소중립저울</span>
+                        </header>
+                  </div>
+                  <div class="item" data-aos="flip-right">
+                        <main class="qr-main">
+                              <div class="qr-done-guide">
+                                    <p class="qr-done-text">배출 완료!</p>
+                              </div>
+                              <div class="qr-done-image-box">
+                                    <img
+                                          class="allsu-logo-3d"
+                                          :src="
+                                                require(`../assets/allsu-logo-3d.png`)
+                                          "
+                                          alt="올수 로고"
+                                    />
+                              </div>
+                              <div class="qr-done-details">
+                                    <span
+                                          >오늘의 배출량은
+                                          <strong>1300mL </strong>이며</span
+                                    >
+                                    <br /><span
+                                          >적립 금액은
+                                          <strong>130원 </strong>입니다</span
+                                    >
+                              </div>
+                              <div class="qr-bottom">
+                                    <button
+                                          class="qr-done-btn"
+                                          @click="[allDone(), $emit('close')]"
+                                    >
+                                          확인
+                                    </button>
+                              </div>
+                        </main>
+                  </div>
             </div>
       </div>
-    </div>
-    <Loading v-if="response === true"/>
-
- 
+      <Loading v-if="response === true" />
 </template>
 <script>
-
-import Loading from './Loading.vue';
+import Loading from "./Loading.vue";
 
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 AOS.init();
 
-
 var timerFunction = "";
 
 export default {
+      components: {
+            Loading,
+      },
 
+      data() {
+            return {
+                  time: 30,
+                  // timer분으로 계산하는 것 만들기
+                  isDone: false,
+                  // 배출완료 state
+                  switch: false,
+                  // 모달창 배출 => 배출 완료 조건부 state
 
-
-
-    components: {
-        Loading,
-    },
-
-
-    data() {
-        return {
-            time: 30,
-            // timer분으로 계산하는 것 만들기
-            isDone: false,
-            // 배출완료 state
-            switch: false,
-            // 모달창 배출 => 배출 완료 조건부 state
-
-            // 시간 state
-            minutes:  0,
-            seconds : "00",
-            counter :  10,
-            response: false,
-            
-        }
-        
-    },
-    
-    methods: {
-
-  
-
-        onClose: function () {
-            console.log(`QR 단순 닫기 for clearInterval`)
-            clearInterval(timerFunction);
-            document.documentElement.style.overflow = "auto"
-
-
-
-        },
-       
-        allDone: function () {
-            console.log(`배출완료 마무리`)
-
-            this.response = true;
-
+                  // 시간 state
+                  minutes: 0,
+                  seconds: "00",
+                  counter: 4,
+                  response: false,
+            };
+      },
+      created() {
+            this.switch = true;
 
             setTimeout(() => {
+                  this.isDone = true;
+            }, 4000);
+      },
+      mounted() {
+            console.log(`마운트`);
+            this.timer();
+      },
 
+      methods: {
+            onClose: function () {
+                  console.log(`QR 단순 닫기 for clearInterval`);
+                  clearInterval(timerFunction);
+                  document.documentElement.style.overflow = "auto";
+            },
 
-                this.response = false;
-                this.switch = false;
+            allDone: function () {
+                  console.log(`배출완료 마무리`);
 
-                // 배출완료 후 스크롤 고정 풀어줌 
+                  this.response = true;
 
+                  setTimeout(() => {
+                        this.response = false;
+                        this.switch = false;
 
+                        // 배출완료 후 스크롤 고정 풀어줌
+                  }, 5000);
 
-            }, 5000)
+                  document.documentElement.style.overflow = "auto";
 
-            document.documentElement.style.overflow = "auto"
+                  // QR 배출 화면으로 초기화
+                  console.log(`배출완료 alldone : ${this.switch}`);
+            },
+            timer() {
+                  timerFunction = setInterval(() => {
+                        this.seconds = --this.counter % 60;
+                        this.minutes = parseInt(this.counter / 60, 10) % 60;
 
+                        if (this.counter <= 0) {
+                              clearInterval(timerFunction);
+                              this.response = true;
 
+                              setTimeout(() => {
+                                    this.response = false;
+                                    this.switch = false;
+                                    document.documentElement.style.overflow =
+                                          "auto";
 
-            // QR 배출 화면으로 초기화 
-            console.log(`배출완료 alldone : ${this.switch}`)
-            
- 
-        },
-        timer() {
-            timerFunction =  setInterval(() => {
-                this.seconds = --this.counter % 60
-                this.minutes = parseInt(this.counter / 60, 10) % 60
- 
+                                    // 배출완료 후 스크롤 고정 풀어줌
+                              }, 5000);
 
-                if (this.counter < 0) {
-                    clearInterval(timerFunction);
-                    this.switch = false;
-                    document.documentElement.style.overflow = "auto"
+                              // 자동으로 완료창으로
+                        }
+                        if (this.switch === false) {
+                              clearInterval(timerFunction);
+                              document.documentElement.style.overflow = "auto";
 
+                              this.switch = false;
+                        }
 
-                    // 자동으로 완료창으로
-                }
-                if (this.switch === false) {
-                    clearInterval(timerFunction);
-                    document.documentElement.style.overflow = "auto"
-
-                     this.switch = false;
-                }
-
-                // 배출 누르고도 interval 안되게 버튼 누르면 clearInterval 종료
-
-                
-
-            }, 1000);
-
-           
-        }
-    },
-    created () {
-        this.switch = true;
-
-        setTimeout(() => {
-
-            this.isDone = true;
-        }, 4000)
-    },  
-    mounted() {
-        console.log(`마운트`)
-        this.timer();
-     
-
-    },
-  
-    
-}
+                        // 배출 누르고도 interval 안되게 버튼 누르면 clearInterval 종료
+                  }, 1000);
+            },
+      },
+};
 </script>
 <style>
+/* QR Background */
+.qr-container {
+      width: 100%;
+      height: 100%;
+      position: fixed;
+      background: rgba(0, 0, 0, 0.43);
+      top: 0;
+      left: 0;
+}
 
+.qr-white-bg {
+      position: fixed;
+      border-radius: 12px;
+      background: white;
+      width: 90%;
+      top: 50%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      -moz-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      -o-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
 
-    /* QR Background */
-    .qr-container {
-        width:100%;
-        height:100%;
-        position: fixed;
-        background: rgba(0,0,0,0.43);
-        top: 0;
-        left: 0;
-        
-       
-    }
+      /*  */
+}
 
-    .qr-white-bg {
-        position: fixed;
-        border-radius: 12px;
-        background: white;
-        width: 90%;
-        top: 50%;
-        left: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        -moz-transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-        -o-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
+/* QR Header */
 
-        /*  */
+.qr-header {
+      position: relative;
+      background-color: #a3cf4d;
+      padding: 15px 50px;
+      text-align: center;
+      color: white;
+      font-size: 22px;
+      font-weight: 600;
+      border-top-left-radius: 13px;
+      border-top-right-radius: 13px;
+      align-items: center;
+      display: flex;
+      justify-content: center;
+}
 
+.qr-header > i {
+      position: absolute;
+      left: 4%;
+}
+.qr-guide {
+      display: flex;
+      justify-content: end;
+      align-items: center;
+      font-size: 14px;
+}
 
-   
-    }
+.qr-quide > span {
+      display: inline-block;
+      margin-bottom: 5px;
+}
 
-    /* QR Header */
+.mdi-information-outline {
+      font-size: 16px;
+}
 
-    
-    
-    .qr-header {
-        position: relative;
-        background-color: #A3CF4D;
-        padding: 15px 50px;
-        text-align: center;
-        color: white;
-        font-size: 22px;
-        font-weight: 600;
-        border-top-left-radius: 13px;
-        border-top-right-radius: 13px;
-        align-items: center;
-        display: flex;
-        justify-content: center;
- 
-       
-    }
+.qr-main {
+      padding: 50px;
+      text-align: center;
+}
 
+.qr-image-box {
+      background: #ffffff;
+      /* Brand Colors/Primary */
 
-    .qr-header > i {
-        position: absolute;
-        left: 4%;
+      border: 4px solid #ffba00;
+      border-radius: 13px;
+}
 
+.qr {
+      width: 100%;
+      height: 100%;
+      margin: 0;
+}
 
-    }
-    .qr-guide {
-         display: flex;
-         justify-content: end;
-         align-items: center;
-         font-size: 14px;
-    }
+/* QR Bottom */
 
-    .qr-quide > span {
-        display: inline-block;
-        margin-bottom: 5px;
-    }
+.qr-timer {
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 35px;
+      margin-top: 15px;
+      margin-bottom: 25px;
 
-    .mdi-information-outline {
-        font-size: 16px;
+      color: #ffba00;
+}
 
-    }
+.qr-timer-red {
+      color: red;
 
-  
-    .qr-main {
-        padding: 50px;
-        text-align: center;
-    }
+      transform: scale(1.2);
+      transition: all ease 2s 0s;
+}
 
-    .qr-image-box {
-        background: #FFFFFF;
-        /* Brand Colors/Primary */
+.qr-progress-btn {
+      background: #a3cf4d;
+      /* Gray Colors/White */
 
-        border: 4px solid #FFBA00;
-        border-radius: 13px;
-    }
+      border: 5px solid #ffffff;
+      box-shadow: 0px 10px 20px 1px rgba(0, 0, 0, 0.1);
+      border-radius: 50px;
+      color: white;
+      padding: 13px 45px;
+      font-size: 20px;
+      font-weight: 600;
+}
 
-    .qr {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-     }
+/* 배출중 버튼 색 */
 
-   
+.qr-done-btn {
+      background: #ffba00;
+      /* Gray Colors/White */
 
+      border: 5px solid #ffffff;
+      box-shadow: 0px 10px 20px 1px rgba(0, 0, 0, 0.1);
+      border-radius: 50px;
+      color: white;
+      padding: 13px 45px;
+      font-size: 20px;
+      font-weight: 600;
+}
+/* 배출완료 버튼 색 */
 
-     /* QR Bottom */
+/* 배출완료 모달 */
 
-   
+.qr-done-text {
+      font-size: 33px;
+      font-weight: 700;
+}
 
-    .qr-timer {
-        font-weight: 400;
-        font-size: 20px;
-        line-height: 35px;
-        margin-top: 15px;
-        margin-bottom: 25px;
-   
-         color: #FFBA00;
- 
-    }
+.qr-done-image-box {
+      width: 50%;
+      height: auto;
+      margin: 0 auto;
+}
 
-    .qr-timer-red {
-        color:red;
-        
-        transform:  scale(1.2);
-        transition: all ease 2s 0s;
+.allsu-logo-3d {
+      width: 100%;
+      height: auto;
+      display: block;
+}
 
-        
-    }
-
-    .qr-progress-btn {
-        background: #A3CF4D;
-    /* Gray Colors/White */
-
-        border: 5px solid #FFFFFF;
-        box-shadow: 0px 10px 20px 1px rgba(0, 0, 0, 0.1);
-        border-radius: 50px;
-        color: white;
-        padding: 13px 45px;
-        font-size: 20px;
-        font-weight: 600;
-    }
-
-    /* 배출중 버튼 색 */
-
-    .qr-done-btn {
-        background: #FFBA00;
-        /* Gray Colors/White */
-
-        border: 5px solid #FFFFFF;
-        box-shadow: 0px 10px 20px 1px rgba(0, 0, 0, 0.1);
-        border-radius: 50px;
-        color: white;
-        padding: 13px 45px;
-        font-size: 20px;
-        font-weight: 600;
-    }
-    /* 배출완료 버튼 색 */
-    
-
-    /* 배출완료 모달 */
-
-    .qr-done-text {
-        font-size: 33px;
-        font-weight: 700;
-    }
-
-    .qr-done-image-box {
-        width: 50%;
-        height: auto;
-        margin: 0 auto;
-
-    }
-
-    .allsu-logo-3d {
-        width: 100%;
-        height: auto;
-        display: block;
-    }
-
-    .qr-done-details {
-        font-size: 13px;
-        color: gray;
-        margin: 10px 0;
-    }
-    .qr-done-details strong {
-        color: black;
-    }
-
- </style>
+.qr-done-details {
+      font-size: 13px;
+      color: gray;
+      margin: 10px 0;
+}
+.qr-done-details strong {
+      color: black;
+}
+</style>
